f9276e92e06f23de45cde24ccbb59e2871c8726a
DOMAIN\hamdi.sevketbeyoglu <hamdi.sevketbeyoglu@softwareone.com>
2022-07-10 01:14:47 +0200

    database layer and repository pattern with mongoDB

---
+++ src/Services/Catalog/CatalogAPI/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿
+using CatalogAPI.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
